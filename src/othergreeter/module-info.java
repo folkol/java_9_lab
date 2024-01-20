b3daaf5181b9fbca4f9a5822680505784a767a60
Tarandeep Singh <tarandeep.singh562@gmail.com>
2024-01-19 17:39:39 -0800

    Update module-info.java

--- src/othergreeter/module-info.java
+++ src/othergreeter/module-info.java
@@ -1,6 +1,5 @@
 module othergreeter {
     requires greeter;
-    exports com.folkol.othergreeter;
     provides com.folkol.greeter.Greeter
         with com.folkol.othergreeter.OtherGreeterImpl;
 }
